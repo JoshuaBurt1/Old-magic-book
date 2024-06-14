5c6329ef8c9adc74c3f1024118110e17de3a212f
Joshua Burt <112965527+JoshuaBurt1@users.noreply.github.com>
2024-06-14 01:11:47 -0400

    Add files via upload

--- DNAbasePairs.c
+++ DNAbasePairs.c
@@ -28,15 +28,15 @@ int main(void) {
 	printf("\n");
 	     
 	// 23:27                                             *pattern: 23/27=0.851...*
-	// "So We revealed"	            W : 1                #pattern: 27/23= 1.17#   
+	// "So We revealed"	      <= Word : 1                #pattern: 27/23= 1.17#   
 	//                             #7 : 1#       
 	//[80]+ 1 + 6 + 8 + [10] + 50 + 1 = 156 
 	//          6,1,50,8,1            : 156,08,01        *order to pattern "158"*
 	//          615.81                : 615.81           *replace 1 with 7; pattern 1.17*
 	//                         615.87 : 615.87           *average DNA base pair mass*
 	//
-	// "the ark"           W : 5
-    //                    #5 : 1#                        *note: average = mesos = 515 (A.Greek); however it is broken 5:1:5*
+	// "the ark"     <= Word : 5
+    //                    #5 : 1#                        *note: average = mesos (A.Greek => 40+5+200+70+200=515) & average = mutawassit (Arabic => 40+400+6+60+9=515); however it is broken 5:1:5*
  	// 1 + 30 + 80 + 30 + 20 = 161  
 	// 1,30,80,30,20         : 615  	 				 *replace 1 with 5*
 	// 1,20                  : 615,03,08,03
@@ -59,15 +59,30 @@ int main(void) {
 	// AT: 331.2213+322.2079-(4x1.0079+2x15.9994)-(2x1.0079)= 615.383
     // average: 615.8771" 
 	// 2. https://onlinelibrary.wiley.com/doi/10.1002/cyto.a.10013
-	//
+	
 	//Contextual reference match:
 	// 1. Various creation stories => "Build the Ark ... take on board a ""pair"" from every species"; 
-	// A. Other old magic book
-	// "Make ... an ark"; Chapter 6, 10th Hebrew patriarch: Noah (Hebrew=50+8) & 1st mention 5:29 (29/5=5.8) => 615.8_  Ref: https://mechon-mamre.org/p/pt/pt0106.htm
-	// B. Translation and ammended version of Other old magic book
-	// "Make ... an ark"; Chapter 6, 10th Hebrew patriarch: Noah (A.Greek=50+800+5) & 1st mention 5:29 (29/5=5.8) => 615.8_  Ref: https://www.blueletterbible.org/lxx/gen/6/1/s_6001
+	// A. Other old magic book                                                                                                                        Ref: https://mechon-mamre.org/p/pt/pt0106.htm                            
+	// "Make ... an ark"; Chapter 6, 10th Hebrew patriarch: Noah (Hebrew => 50 + 8 = 58) & 1st mention 5:29 (29/5=5.8) => 615.8_                      *note: 5+29=34 : 1 sigma flag*
+	// First words including "Noah" are [And he called his name Noah] 
+	//                        (Hebrew => 6 + 10 + 100 + 200 + 1 + 1 + 400 + 300 + 40 + 6 + 50 + 8 = 1122)                                              *use values: 1,2,11,12*
+	//                                                                                                                                                              6,10,50,8
+	//                        6,10        (100+200+1+1+400+300+40+6 = 1048 : similar to Hebrew word unique : yachid = 10 + 8 + 10 + 4 = 32)      50,8  => 615.8
+	//                        6,10        *unique [1] values of 8 are 7 & 1122/(letter per word) = 1122/(5*2*3*2) = 18.7*                        50,8  => 615.8
+	//                        6,10        *7 unique values : 1048/32 = 32.75 => yachid, 7 , next value 5 & next value 5th digit*                 50,8  => 615.87
+	//                                    *12 letters with value, 4 words, 1 sentence : 12+4+1 = 17 & 12*4*1=48= 7*7-1 *                               => 615.8771
+	// Note: First "word" of 5:29 is English translastion "And" = Vav + yod (Hebrew => 6 + 10) : cell nuclear material conveiniently called [DNA]                   
+	//                
+	// B. Translation and ammended version of Other old magic book                                                                                     Ref: https://www.blueletterbible.org/lxx/gen/6/1/s_6001
+	// "Make ... an ark"; Chapter 6, 10th Hebrew patriarch: Noah (A.Greek=50+800+5) & 1st mention 5:29 (29/5=5.8) => 615.8_
+	// First words including "Noah" are [And he called his name Noah]                                                                                  
+	//                        (A.Greek => ((20 + 1 + 10) + (5 + 80 + 800 + 50 + 70 + 40 + 1 + 200 + 5 + 50) + (300 + 70) + ( 70 + 50 + 70 + 40 + 1) + (1 + 400 + 300 + 70 + 400) + (50 + 800 + 5) = 3959))        
+	//                        *This seems to have lost its link to the value 615.87 through direct translation. The Hebrew version is carefully crafted.*
+	//                        *Only through Hebrew and Ancient Greek's shared link to proto-indo-european: Noah N value of 50 is retained*
+	//             
 	// 2. Voynich Manuscript: page 75, 1st "DNA page", 6 & 8 nymphs in divided pools => 615.87
 	// 3. Dune (1[9]65): https://www.youtube.com/watch?v=124xCHfVUk4
+	
 	                                                
 	printf("Question 3: \n");
 	int numBasePair = 615;
